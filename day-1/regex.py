110859a52c80b5ce1267b479701a39d0c3059ca2
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-09-08 17:29:38 +0530

    finished regex

--- day-1/regex.py
+++ day-1/regex.py
@@ -40,36 +40,92 @@
 
 
 # special sequences
+# import re
+
+# a = "harry potter and the1 chamber2@34 of secrets"
+
+# match = re.search(r"\Aharry",a)
+# print(match)
+
+# match = re.search(r"ha\B",a)
+# print(match)
+
+# match = re.search(r"\ber",a)
+# print(match)
+
+# match = re.findall(r"\d",a)
+# print(match)
+
+# match = re.findall(r"\D",a)
+# print(match)
+
+# match = re.findall(r"\s",a)
+# print(match)
+
+# match = re.findall(r"\S",a)
+# print(match)
+
+# match = re.findall(r"\w",a)
+# print(match)
+
+# match = re.findall(r"\W",a)
+# print(match)
+
+# match = re.findall(r"\Z",a)
+# print(match)
+
+# re.findall()
+# import re
+# a = """ John has scored 89 marks
+# lisa has scored 58 marks
+# david has scored 78 marks """
+# print(re.findall(r"\d+",a))
+# print(re.findall(r"[A-Z][a-z]*",a))
+
+# re.compile()
+# import re
+# a = """ John has scored 89 marks
+# lisa has scored 58 marks
+# david has scored 78 marks """
+# p = re.compile("[a-d]")
+# p = re.compile("[0-9]")
+# print(re.findall(p,a))
+
+# re.split()
+# import re
+# a = """ John has scored 89 marks
+# lisa has scored 58 marks
+# david has scored 78 marks """
+# b = "fantastic 5 turtles"
+# print(re.split(r"\d+" ,a))
+# print(re.split(r"\d+" ,b))
+
+# re.sub()
+# import re
+# a = """ John has scored 89 marks
+# lisa has scored 58 marks
+# # david has scored 78 marks """
+# b = "fantastic 5 turtles"
+# print(re.sub(r"\s+", "", a))
+# print(re.sub(r"\s+", "", b))
+
+# re.subn()
+# import re
+# a = """ John has scored 89 marks
+# lisa has scored 58 marks
+# # david has scored 78 marks """
+# print(re.subn(r"\s+", "", a))
+
+# re.escape()
+# import re
+# a = """ John has scored 89 marks
+# lisa has scored 58 marks
+# # david has scored 78 marks """
+# print(re.escape(a))
+
+# re.search()
 import re
-
-a = "harry potter and the1 chamber2@34 of secrets"
-
-match = re.search(r"\Aharry",a)
-print(match)
-
-match = re.search(r"ha\B",a)
-print(match)
-
-match = re.search(r"\ber",a)
-print(match)
-
-match = re.findall(r"\d",a)
-print(match)
-
-match = re.findall(r"\D",a)
-print(match)
-
-match = re.findall(r"\s",a)
-print(match)
-
-match = re.findall(r"\S",a)
-print(match)
-
-match = re.findall(r"\w",a)
-print(match)
-
-match = re.findall(r"\W",a)
-print(match)
-
-match = re.findall(r"\Z",a)
-print(match)
+a = """ John has scored 89 marks
+lisa has scored 58 marks
+# david has scored 78 marks """
+print(re.search(r"\d+", a))
