3ee90b6aeb4c1b1c991758075e0dd948d4ab8761
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-09-10 17:09:34 +0530

    finished serialized

--- day-2/serialization.py
+++ day-2/serialization.py
@@ -1,41 +1,41 @@
 # Serialization
 
-# import json
-# L = [
-#     {
-#         "tradeid": 1, "symbol": "AAPL", "price": 150.0, "volume": 1234
-#     }, 
-#     {
-#         "tradeid": 2, "symbol": "GOOGL", "price": 2800.5, "volume": 567
-#     },
-#     {
-#         "tradeid": 3, "symbol": "MSFT", "price": 300.75, "volume": 890
-#     },
-#     {
-#         "tradeid": 4, "symbol": "AMZN", "price": 3400.0, "volume": 345
-#     },
-#     {
-#         "tradeid": 5, "symbol": "TSLA", "price": 700.25, "volume": 678
-#     }
-# ]
+import json
+L = [
+    {
+        "tradeid": 1, "symbol": "AAPL", "price": 150.0, "volume": 1234
+    }, 
+    {
+        "tradeid": 2, "symbol": "GOOGL", "price": 2800.5, "volume": 567
+    },
+    {
+        "tradeid": 3, "symbol": "MSFT", "price": 300.75, "volume": 890
+    },
+    {
+        "tradeid": 4, "symbol": "AMZN", "price": 3400.0, "volume": 345
+    },
+    {
+        "tradeid": 5, "symbol": "TSLA", "price": 700.25, "volume": 678
+    }
+]
 
-# with open('trades.json', 'w') as f:
-#     json.dump(L, f, indent=4)
+with open('trades.json', 'w') as f:
+    json.dump(L, f, indent=4)
     
 # Deserialization
 
-# import json
-# students = '{"id": "01", "name": "Riddhi", "department": "Computer Science "}'
-# student_dict = json.loads(students)
-# print(student_dict)
-# print(student_dict['name'])  # Accessing name from the dictionary
-# print("Deserialization complete.")
+import json
+students = '{"id": "01", "name": "Riddhi", "department": "Computer Science "}'
+student_dict = json.loads(students)
+print(student_dict)
+print(student_dict['name'])  # Accessing name from the dictionary
+print("Deserialization complete.")
 
 # # Serialization and Deserialization tuple
 
-# t = ("T1234", "AAPL", 150.0)
-# with open('trade_tuple.json', 'w') as f:
-#     json.dump(t, f)
+t = ("T1234", "AAPL", 150.0)
+with open('trade_tuple.json', 'w') as f:
+    json.dump(t, f)
 
 # Serialize and Deserialize an Open File Object Using Pickle
 
