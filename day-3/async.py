a3d00511a7b3b60c946c63f5f0c3993e59f10bd5
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-09-11 15:16:53 +0530

    finished asyncio

--- day-3/async.py
+++ day-3/async.py
@@ -1,99 +1,100 @@
-# # Asynchronous Programming with Asyncio in Python
+# Asynchronous Programming with Asyncio in Python
 
-# import asyncio 
+import asyncio 
 
-# async def fn():
-#     print("This is")
-#     await asyncio.sleep(1)
-#     print("asynchronous Programming")
-#     await asyncio.sleep(1)
-#     print("and not multi-threading")
-# asyncio.run(fn())
+async def fn():
+    print("This is")
+    await asyncio.sleep(1)
+    print("asynchronous Programming")
+    await asyncio.sleep(1)
+    print("and not multi-threading")
 
-# # Async Event Loop in Python
+asyncio.run(fn())
 
-# import asyncio
+# Async Event Loop in Python
 
-# async def fn():
-#     print("one")
-#     await asyncio.sleep(1)
-#     await fn2()
-#     print('four')
-#     await asyncio.sleep(1)
-#     print('five')
-#     await asyncio.sleep(1)
+import asyncio
 
-# async def fn2():
-#     await asyncio.sleep(1)
-#     print("two")
-#     await asyncio.sleep(1)
-#     print("three")
+async def fn():
+    print("one")
+    await asyncio.sleep(1)
+    await fn2()
+    print('four')
+    await asyncio.sleep(1)
+    print('five')
+    await asyncio.sleep(1)
 
-# asyncio.run(fn())
+async def fn2():
+    await asyncio.sleep(1)
+    print("two")
+    await asyncio.sleep(1)
+    print("three")
 
-# # asyncio.create_task(fn2())
+asyncio.run(fn())
 
-# import asyncio 
+# asyncio.create_task(fn2())
 
-# async def fn():
-#     task = asyncio.create_task(fn2())
-#     print("one")    # run num 1
-#     # await asyncio.sleep(1)
-#     # await fn2()
-#     print("four")   # run num 2
-#     await asyncio.sleep(1)
-#     print("five")   # run num 4
-#     await asyncio.sleep(1)
+import asyncio 
 
-# async def fn2():
-#     # await asyncio.sleep(1)
-#     print("tow")    # run num 3
-#     await asyncio.sleep(1)
-#     print("three")  # run num 5
+async def fn():
+    task = asyncio.create_task(fn2())
+    print("one")    # run num 1
+    # await asyncio.sleep(1)
+    # await fn2()
+    print("four")   # run num 2
+    await asyncio.sleep(1)
+    print("five")   # run num 4
+    await asyncio.sleep(1)
 
-# asyncio.run(fn())
+async def fn2():
+    # await asyncio.sleep(1)
+    print("tow")    # run num 3
+    await asyncio.sleep(1)
+    print("three")  # run num 5
 
-# # I/O-bound tasks using asyncio.sleep()
+asyncio.run(fn())
 
-# import asyncio 
+# I/O-bound tasks using asyncio.sleep()
 
-# async def func1():
-#     print("Function 1 started..")
-#     await asyncio.sleep(2)
-#     print("Function 1 ended")
+import asyncio 
 
-# async def func2():
-#     print("Function 2 started..")
-#     await asyncio.sleep(3)
-#     print("Function 2 ended")
+async def func1():
+    print("Function 1 started..")
+    await asyncio.sleep(2)
+    print("Function 1 ended")
 
-# async def func3():
-#     print("Function 3 started..")
-#     await asyncio.sleep(1)
-#     print("Function 3 ended")
+async def func2():
+    print("Function 2 started..")
+    await asyncio.sleep(3)
+    print("Function 2 ended")
 
-# async def main():
-#     L = await asyncio.gather(
-#         func1(),
-#         func2(),
-#         func3(),
-#     )
-#     print("main ended")
+async def func3():
+    print("Function 3 started..")
+    await asyncio.sleep(1)
+    print("Function 3 ended")
+
+async def main():
+    L = await asyncio.gather(
+        func1(),
+        func2(),
+        func3(),
+    )
+    print("main ended")
     
-# asyncio.run(main())
+asyncio.run(main())
 
-# # Coroutines
+# Coroutines
 
-# import asyncio
+import asyncio
 
-# async def main():
-#     return 32
+async def main():
+    return 32
 
-# async def main():
-#     nested()    # will raise a RunTimeWarning
-#     print(await nested())   # will print 32
+async def main():
+    nested()    # will raise a RunTimeWarning
+    print(await nested())   # will print 32
 
-# asyncio.run(main())   # this code giv me a error
+asyncio.run(main())   # this code giv me a error
 
 # tasks
 
@@ -106,4 +107,27 @@ async def main():
     task = asyncio.create_task(nested())
     await task
 
+asyncio.run(main())
+
+# Running tasks concurrently
+
+import asyncio
+
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number + 1):
+        print(f"Task {name}: Compute factorial({number}), currently i = {i}...")
+        await asyncio.sleep(1)
+        f *= i
+    print(f"Task {name}: factorial({number}) = {f}")
+    return f
+
+async def main():
+    L = await asyncio.gather(
+        factorial("A", 2),
+        factorial("B", 3),
+        factorial("C", 4),
+    )
+    print(L)
+    
 asyncio.run(main())
