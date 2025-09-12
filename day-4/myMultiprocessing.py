203de9868b28f458db91f3a3fad14b772603d69e
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-09-12 18:17:32 +0530

    finished Multiprocessing

---
+++ day-4/myMultiprocessing.py
@@ -0,0 +1,98 @@
+# import time, os
+# from threading import Thread, current_thread
+# from multiprocessing import Process, current_process
+
+# COUNT = 200000000
+# SLEEP = 10
+
+# def io_bound(sec):
+
+#     pid = os.getpid()
+#     threadName = current_thread().name
+#     processName = current_process().name
+
+#     print(f"{pid} * {processName} * {threadName} \
+#         ---> Start sleeping...")
+#     time.sleep(sec)
+#     print(f"{pid} * {processName} * {threadName} \
+#         ---> Finished sleeping...")
+
+# def cpu_bound(n):
+
+#     pid = os.getpid()
+#     threadName = current_thread().name
+#     processName = current_process().name
+
+#     print(f"{pid} * {processName} * {threadName} \
+#         ---> Start counting...")
+
+#     while n>0:
+#         n -= 1
+
+#     print(f"{pid} * {processName} * {threadName} \
+#         ---> Finished counting...")
+
+# if __name__=="__main__":
+#     start = time.time()
+
+#     end = time.time()
+#     print('Time taken in seconds -', end - start)
+    
+# # Example of Multi-threading and Multiprocessing using Python
+
+# # ThreadPoolExecutor Example
+# import concurrent.futures
+# import urllib.request
+
+# URLS = ['http://www.foxnews.com/',
+#         'http://www.cnn.com/',
+#         'http://europe.wsj.com/',
+#         'http://www.bbc.co.uk/',
+#         'http://some-made-up-domain.com/']
+
+# def load_url(url, timeout):
+#     with urllib.request.urlopen(url, timeout=timeout) as conn:
+#         return conn.read()
+
+# # We can use a with statement to ensure threads are cleaned up promptly
+# with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+#     # Start the load operations and mark each future with its URL
+#     future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
+#     for future in concurrent.futures.as_completed(future_to_url):
+#         url = future_to_url[future]
+#         try:
+#             data = future.result()
+#         except Exception as exc:
+#             print('%r generated an exception: %s' % (url, exc))
+#         else:
+#             print('%r page is %d bytes' % (url, len(data)))
+
+# ProcessPoolExecutor
+import concurrent.futures
+import math
+
+PRIMES = [
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    1157978480770995,
+    5468797979431649]
+
+def is_prime(n):
+    if n % 2 == 0:
+        return False
+
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def main():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('%d is prime: %s' % (number, prime))
+
+if __name__ == '__main__':
+    main()
