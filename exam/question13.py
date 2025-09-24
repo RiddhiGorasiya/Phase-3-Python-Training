7022a582e1657eb5bd372bade046b37e958933b2
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-09-24 15:14:02 +0530

    complet question13.py

--- exam/question13.py
+++ exam/question13.py
@@ -4,7 +4,6 @@ def extract_phone_numbers(text):
     pattern = r"\b\d{3}[-.\s]??\d{3}[-.\s]??\d{4}\b"
     return re.findall(pattern, text)
 
-# Example usage:
 text = "Call me at 123-456-7890 or 987.654.3210 or 555 666 7777."
 print(extract_phone_numbers(text))
 
